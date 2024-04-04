f1b36e420ae9381b64fbe8d40eb4f241687a5644
Ziani Meryem <meryem.ziani.etu@a10p4.fil.univ-lille.fr>
2024-04-04 15:06:15 +0200

    rename the variable fTimesRepeat with repeatCount

--- src/main/java/junit/extensions/RepeatedTestCaseDecorator.java
+++ src/main/java/junit/extensions/RepeatedTestCaseDecorator.java
@@ -7,24 +7,24 @@ import junit.framework.TestResult;
  * A Decorator that runs a test repeatedly.
  */
 public class RepeatedTestCaseDecorator extends TestDecorator {
-    private int fTimesRepeat;
+    private int repeatCount;
 
     public RepeatedTestCaseDecorator(Test test, int repeat) {
         super(test);
         if (repeat < 0) {
             throw new IllegalArgumentException("Repetition count must be >= 0");
         }
-        fTimesRepeat = repeat;
+        repeatCount = repeat;
     }
 
     @Override
     public int countTestCases() {
-        return super.countTestCases() * fTimesRepeat;
+        return super.countTestCases() * repeatCount;
     }
 
     @Override
     public void run(TestResult result) {
-        for (int i = 0; i < fTimesRepeat; i++) {
+        for (int i = 0; i < repeatCount; i++) {
             if (result.shouldStop()) {
                 break;
             }
